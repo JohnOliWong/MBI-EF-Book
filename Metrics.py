dbec84b82699a26845b6a94a23d095e097cc9780
JohnOliWong <software@freedomsino.com>
2025-06-17 20:18:45 +0800

    3.2b
    Added one parameter log_name in the metrics function

--- Metrics.py
+++ Metrics.py
@@ -3,7 +3,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, cohen_kappa
 import pandas as pd
 import os
 
-def log_metrics(subject, log_mode, acc_list, precision_list, recall_list, f1_list, kappa_list):
+def log_metrics(subject, log_name, log_mode, acc_list, precision_list, recall_list, f1_list, kappa_list):
 		mean_acc, std_acc = np.mean(acc_list[-50:]), np.std(acc_list[-50:])
 		mean_precision, std_precision = np.mean(precision_list[-50:]), np.std(precision_list[-50:])
 		mean_recall, std_recall = np.mean(recall_list[-50:]), np.std(recall_list[-50:])
@@ -17,7 +17,7 @@ def log_metrics(subject, log_mode, acc_list, precision_list, recall_list, f1_lis
 		mean_f1, std_f1 = mean_f1 * 100, std_f1 * 100
 	
 		if log_mode in [0, 2]:
-			log_path = f'Results/log_s{subject:2d}.txt'
+			log_path = f'Results/Subject_{subject}.txt'
 			with open(log_path, 'a') as log_file:
 				log_file.write(f'Accuracy: {mean_acc:.2f} ± {std_acc:.2f}\n')
 				log_file.write(f'Precision: {mean_precision:.2f} ± {std_precision:.2f}\n')
@@ -28,7 +28,7 @@ def log_metrics(subject, log_mode, acc_list, precision_list, recall_list, f1_lis
 				log_file.write('\n')
 
 		if log_mode in [1, 2]:
-			log_excel = 'Results/Log.xlsx'
+			log_excel = 'Results/' + str(log_name) + '.xlsx'
 			new_row = pd.DataFrame([[mean_acc, std_acc, mean_precision, std_precision, mean_recall, std_recall, mean_f1, std_f1, mean_kappa]], 
 								columns=['Accuracy', 'Std_Accuracy', 'Precision', 'Std_Precision', 'Recall', 'Std_Recall', 'F1', 'Std_F1', 'Kappa'])
 			new_row = new_row.round(2)

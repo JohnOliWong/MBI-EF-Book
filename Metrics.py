da787e264c8c594954d20edafc6ba19d30df682a
JohnOliWong <software@freedomsino.com>
2025-06-18 21:50:07 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -3,7 +3,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, cohen_kappa
 import pandas as pd
 import os
 
-def log_metrics(subject, log_name, log_mode, acc_list, precision_list, recall_list, f1_list, kappa_list):
+def metrics(subject, log_name, log_mode, acc_list, precision_list, recall_list, f1_list, kappa_list):
 		mean_acc, std_acc = np.mean(acc_list[-50:]), np.std(acc_list[-50:])
 		mean_precision, std_precision = np.mean(precision_list[-50:]), np.std(precision_list[-50:])
 		mean_recall, std_recall = np.mean(recall_list[-50:]), np.std(recall_list[-50:])
@@ -16,8 +16,10 @@ def log_metrics(subject, log_name, log_mode, acc_list, precision_list, recall_li
 		mean_recall, std_recall = mean_recall * 100, std_recall * 100
 		mean_f1, std_f1 = mean_f1 * 100, std_f1 * 100
 	
+		results_dir = 'Results/'
+		log_name = str(log_name)
 		if log_mode in [0, 2]:
-			log_path = f'Results/Subject_{subject}.txt'
+			log_path = 'Results/' + log_name + f'Subject_{subject}.txt'
 			with open(log_path, 'a') as log_file:
 				log_file.write(f'Accuracy: {mean_acc:.2f} ± {std_acc:.2f}\n')
 				log_file.write(f'Precision: {mean_precision:.2f} ± {std_precision:.2f}\n')
@@ -28,7 +30,10 @@ def log_metrics(subject, log_name, log_mode, acc_list, precision_list, recall_li
 				log_file.write('\n')
 
 		if log_mode in [1, 2]:
-			log_excel = 'Results/' + str(log_name) + '.xlsx'
+			log_root = results_dir + log_name
+			if not os.path.exists(log_root):
+				os.makedirs(log_root)
+			log_excel = log_root + f'/{log_name}.xlsx'
 			new_row = pd.DataFrame([[mean_acc, std_acc, mean_precision, std_precision, mean_recall, std_recall, mean_f1, std_f1, mean_kappa]], 
 								columns=['Accuracy', 'Std_Accuracy', 'Precision', 'Std_Precision', 'Recall', 'Std_Recall', 'F1', 'Std_F1', 'Kappa'])
 			new_row = new_row.round(2)

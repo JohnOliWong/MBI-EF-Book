82605b51f039ca4b7d1f50328bf88d86e4d0acee
JohnOliWong <software@freedomsino.com>
2025-07-10 16:00:49 +0800

    7.10

--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
@@ -7,6 +8,7 @@ class Visualization():
 	def __init__(self, subject, mode, results_root):
 		self.subject = str(subject)
 		self.title = ('MI' if mode == 0 else 'MA' if mode == 1 else 'WG')
+		self.results_root = results_root
 		self.curve_path = results_root + f'Curve_{self.subject}_{self.title}.png'
 		self.cm_path = results_root + f'CM_{self.subject}_{self.title}.png'
 
@@ -27,13 +29,14 @@ class Visualization():
 		epochs = np.arange(1, len(train_loss)+1, dtype=np.int32)
 		
 		ax1.set_xlabel('Epoch')
-		# ax1.set_xlim(min(epochs) - 0.5, max(epochs) + 0.5)
+		ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
 		ax1.set_ylabel('Loss', color='tab:blue')
 		line1 = ax1.plot(epochs, train_loss, 'b-o', label='Training_loss')
 		line2 = ax1.plot(epochs, eval_loss, 'b--s', label='Testing_Loss')
 		ax1.tick_params(axis='y', labelcolor='tab:blue')
 		
 		ax2 = ax1.twinx()
+		ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
 		ax2.set_ylabel('Accuracy (%)', color='tab:red')
 		line3 = ax2.plot(epochs, train_acc, 'r-^', label='Training_Acc')
 		line4 = ax2.plot(epochs, eval_acc, 'r--D', label='Testing_Acc')
@@ -50,12 +53,23 @@ class Visualization():
 	
 	def plot_cm(self, all_labels, all_preds):
 		cm = confusion_matrix(all_labels, all_preds)
+		cm = cm / cm.sum(axis=1, keepdims=True)
 		disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Left', 'Right'])
 		disp.plot(cmap=plt.cm.Blues)
 		plt.title(self.title)
 		plt.savefig(self.cm_path, dpi=300, bbox_inches='tight')
 		plt.close()
 	
+	def plot_cm_all(self, all_labels, all_preds):
+		cm = confusion_matrix(all_labels, all_preds)
+		cm = cm / cm.sum(axis=1, keepdims=True)
+		disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Left', 'Right'])
+		disp.plot(cmap=plt.cm.Blues)
+		plt.title(self.title)
+		cm_save_path = self.results_root + f'CM_Overall_{self.title}.png'
+		plt.savefig(self.cm_path, dpi=300, bbox_inches='tight')
+		plt.close()
+		
 	def plot_all(self, train_loss, train_acc, eval_loss, eval_acc, all_labels, all_preds):
 		self.plot_curve(train_loss, train_acc, eval_loss, eval_acc)
 		self.plot_cm(all_labels, all_preds)

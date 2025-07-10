3245d1c7b4fb8af7fcac6986ff0452d358efcd3b
JohnOliWong <software@freedomsino.com>
2025-07-10 15:29:06 +0800

    7.10
    Changed how the metrics are recorded, added the visualization tools

--- Metrics.py
+++ Metrics.py
@@ -4,26 +4,12 @@ import pandas as pd
 import os
 
 
-def train_metrics(log_root, log_name, acc_list, precision_list, recall_list, f1_list, kappa_list):
-	if not os.path.exists(log_root):
-		os.makedirs(log_root)
-	log_excel = log_root + f'{log_name}.xlsx'
-	acc_list = [acc * 100 for acc in acc_list]
-	precision_list = [precision * 100 for precision in precision_list]
-	recall_list = [recall * 100 for recall in recall_list]
-	f1_list = [f1 * 100 for f1 in f1_list]
-	data = {
-		'Accuracy': acc_list,
-		'Precision': precision_list,
-		'Recall': recall_list,
-		'F1': f1_list,
-		'Kappa': kappa_list
-	}
+def metrics(log_root, data):
 	new_row = pd.DataFrame(data).round(2)
-	if not os.path.exists(log_excel):
-		new_row.to_excel(log_excel, index=False, engine='openpyxl')
+	if not os.path.exists(log_root):
+		new_row.to_excel(log_root, index=False, engine='openpyxl')
 	else:
-		with pd.ExcelWriter(log_excel, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
+		with pd.ExcelWriter(log_root, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
 			if 'Sheet1' in writer.sheets:
 				startrow = writer.sheets['Sheet1'].max_row
 			else:
@@ -32,40 +18,11 @@ def train_metrics(log_root, log_name, acc_list, precision_list, recall_list, f1_
 	
 	print('Logging Completed')
 
-def eval_metrics(log_root, acc_list, precision_list, recall_list, f1_list, kappa_list):
-	if not os.path.exists(log_root):
-		os.makedirs(log_root)
-	exp_name = log_root.split('/')[-2]
-	log_excel = log_root + f'{exp_name}.xlsx'
-	acc_list = [acc * 100 for acc in acc_list]
-	precision_list = [precision * 100 for precision in precision_list]
-	recall_list = [recall * 100 for recall in recall_list]
-	f1_list = [f1 * 100 for f1 in f1_list]
-	data = {
-		'Accuracy': acc_list,
-		'Precision': precision_list,
-		'Recall': recall_list,
-		'F1': f1_list,
-		'Kappa': kappa_list
-	}
-	new_row = pd.DataFrame(data).round(2)
-	if not os.path.exists(log_excel):
-		new_row.to_excel(log_excel, index=False, engine='openpyxl')
-	else:
-		with pd.ExcelWriter(log_excel, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
-			if 'Sheet1' in writer.sheets:
-				startrow = writer.sheets['Sheet1'].max_row
-			else:
-				startrow = 0
-			new_row.to_excel(writer, index=False, header=False, startrow=startrow)
-	
-	print('Logging Completed')
-
-def save_seed(exp_name, mode, seeds):
+def save_seed(results_root, mode, seeds):
 	log_root = 'Results/' + 'Seed.txt'
 
 	with open(log_root, 'w') as f:
-		f.write(exp_name + '\n')
+		f.write(results_root + '\n')
 		f.write(str(mode) + '\n')
 		for seed in seeds:
 			f.write(str(seed) + '\n')
@@ -75,8 +32,8 @@ def load_seed():
 
 	with open(log_root, 'r') as f:
 		lines = f.readlines()
-		exp_name = lines[0].strip()
+		results_root = lines[0].strip()
 		mode = int(lines[1].strip())
-		seeds = [int(line.strip()) for line in lines[1:]]
+		seeds = [int(line.strip()) for line in lines[2:]]
 
-	return exp_name, mode, seeds
+	return results_root, mode, seeds
---
+++ Visualization.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+
+class Visualization():
+	def __init__(self, subject, mode, results_root):
+		self.subject = str(subject)
+		self.title = ('MI' if mode == 0 else 'MA' if mode == 1 else 'WG')
+		self.curve_path = results_root + f'Curve_{self.subject}_{self.title}.png'
+		self.cm_path = results_root + f'CM_{self.subject}_{self.title}.png'
+
+	def _get_title(self):
+		return self.title
+	
+	def _get_save_path(self):
+		return self.curve_path, self.cm_path
+
+	def plot_curve(self, train_loss, train_acc, eval_loss, eval_acc):
+		fig, ax1 = plt.subplots(figsize=(12, 7))
+		plt.title(self.title, fontsize=16)
+		
+		train_loss = np.array(train_loss)
+		train_acc = np.array(train_acc)
+		eval_loss = np.array(eval_loss)
+		eval_acc = np.array(eval_acc)
+		epochs = np.arange(1, len(train_loss)+1, dtype=np.int32)
+		
+		ax1.set_xlabel('Epoch')
+		# ax1.set_xlim(min(epochs) - 0.5, max(epochs) + 0.5)
+		ax1.set_ylabel('Loss', color='tab:blue')
+		line1 = ax1.plot(epochs, train_loss, 'b-o', label='Training_loss')
+		line2 = ax1.plot(epochs, eval_loss, 'b--s', label='Testing_Loss')
+		ax1.tick_params(axis='y', labelcolor='tab:blue')
+		
+		ax2 = ax1.twinx()
+		ax2.set_ylabel('Accuracy (%)', color='tab:red')
+		line3 = ax2.plot(epochs, train_acc, 'r-^', label='Training_Acc')
+		line4 = ax2.plot(epochs, eval_acc, 'r--D', label='Testing_Acc')
+		ax2.tick_params(axis='y', labelcolor='tab:red')
+		ax2.set_ylim(0, 100) 
+		
+		lines = line1 + line2 + line3 + line4
+		labels = [l.get_label() for l in lines]
+		ax1.legend(lines, labels, loc='upper center', ncol=2, frameon=True, shadow=True)
+		ax1.grid(True, linestyle='--', alpha=0.7)
+		
+		plt.tight_layout()
+		plt.savefig(self.curve_path, dpi=300, bbox_inches='tight')
+	
+	def plot_cm(self, all_labels, all_preds):
+		cm = confusion_matrix(all_labels, all_preds)
+		disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Left', 'Right'])
+		disp.plot(cmap=plt.cm.Blues)
+		plt.title(self.title)
+		plt.savefig(self.cm_path, dpi=300, bbox_inches='tight')
+		plt.close()
+	
+	def plot_all(self, train_loss, train_acc, eval_loss, eval_acc, all_labels, all_preds):
+		self.plot_curve(train_loss, train_acc, eval_loss, eval_acc)
+		self.plot_cm(all_labels, all_preds)

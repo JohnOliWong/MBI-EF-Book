d563d8918e1833373b5eade20014f4ebea709a0b
JohnOliWong <software@freedomsino.com>
2025-08-15 23:21:28 +0800

    Update Exp.py

--- Exp.py
+++ Exp.py
@@ -1,13 +1,22 @@
 import numpy as np
 import torch
 import os
+from pathlib import Path
 
 
-exp_name = '8008/'
-results_root = 'Results/' + exp_name
-print(results_root)
-exp_name = results_root.split('/')[-2]
-print(exp_name)
+# exp_name = '8008/'
+# results_root = 'Results/' + exp_name
+# print(results_root)
+# exp_name = results_root.split('/')[-2]
+# print(exp_name)
 
-attachments = ('nohup.out' if os.path.exists('nohup.out') else None)
-print(attachments)
+# attachments = ('nohup.out' if os.path.exists('nohup.out') else None)
+# print(attachments)
+
+data_root = 'D:/HIT/MBI/1.xlsx'
+data_root = Path(data_root)
+data_root = str(data_root)
+data_root = data_root.split('\\')
+print(data_root)
+new_root = '/'.join(data_root[:-1]) + '/'
+print(new_root)

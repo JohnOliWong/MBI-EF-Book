0909d16d6a87ee7e3e130a50af8f68021dfbb129
JohnOliWong <software@freedomsino.com>
2025-12-22 00:13:53 +0800

    Update Exp.py

--- Exp.py
+++ Exp.py
@@ -2,9 +2,10 @@ import numpy as np
 import torch
 import os
 from pathlib import Path
-from collections import Counter
+from collections import defaultdict, Counter
 import bisect
 from itertools import accumulate
+import datetime
 
 
 # exp_name = '8008/'
@@ -177,19 +178,143 @@ from itertools import accumulate
 # 	count += (interval - 1) // 2
 # print(count)
 
-nums = [1,1,1,2,2,3]
-k = 2
-hash_map = {}
-for num in nums:
-	hash_map[num] = hash_map.get(num, 0) + 1
+# nums = [1,1,1,2,2,3]
+# k = 2
+# hash_map = {}
+# for num in nums:
+# 	hash_map[num] = hash_map.get(num, 0) + 1
 
-keys = hash_map.keys()
-index_map = {}
-for key in keys:
-	index_map[hash_map[key]] = key
+# keys = hash_map.keys()
+# index_map = {}
+# for key in keys:
+# 	index_map[hash_map[key]] = key
 
-index_list = list(index_map.keys())
-index_list.sort()
-res = [0] * k
-for i in range(k):
-	res[i] = index_map[index_list[i]]
+# index_list = list(index_map.keys())
+# index_list.sort()
+# res = [0] * k
+# for i in range(k):
+# 	res[i] = index_map[index_list[i]]
+
+# grid = [[1,3,1],[1,5,1],[4,2,1]]
+# m, n = len(grid), len(grid[0])
+# dp = [[0] * n for _ in range(m)]
+# val = 0
+# for j in range(n):
+# 	val += grid[0][j]
+# 	dp[0][j] = val
+# val = 0
+# for i in range(m):
+# 	val += grid[i][0]
+# 	dp[i][0] = val
+
+# for i in range(1, m):
+# 	for j in range(1, n):
+# 		dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+# print(dp)
+
+# matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+# target = 5
+# m, n = len(matrix), len(matrix[0])
+
+# def row_bisect(row):
+# 	left, right = 0, n-1
+# 	while left <= right:
+# 		mid = left + (right - left) // 2
+# 		if matrix[row][mid] < target:
+# 			left += 1
+# 		elif matrix[row][mid] > target:
+# 			right -= 1
+# 		else:
+# 			return mid
+# 	return left
+# col = row_bisect(0)
+# col -= 1
+# print(col)
+
+# def col_bisect(col):
+# 	left, right = 0, m-1
+# 	while left <= right:
+# 		mid = left + (right - left) // 2
+# 		if matrix[mid][col] < target:
+# 			left += 1
+# 		elif matrix[mid][col] > target:
+# 			right -= 1
+# 		else:
+# 			return mid
+# 	return left
+# row = col_bisect(col)
+# print(matrix[row][col] == target)
+
+# s = 'bb'
+# n = len(s)
+# dp = [[0] * n for _ in range(n+1)]
+# for i in range(n):
+# 	dp[i][i] = 1
+
+# res = 1
+# ps = s[0]
+# for i in range(1, n):
+# 	for j in range(i):
+# 		print(i, j)
+# 		if s[i] == s[j]:
+# 			dp[i][j] = dp[i-1][j+1] + 2
+# 		else:
+# 			dp[i][j] = dp[i-1][j+1]
+# 		if dp[i][j] > res:
+# 			res = dp[i][j]
+# 			ps = s[j:i+1]
+# print(dp)
+
+
+# from collections import defaultdict
+
+# n = 3
+# buildings = [[1,2],[2,2],[3,2],[2,1],[2,3]]
+# n = 5
+# buildings = [[1,3],[3,2],[3,3],[3,5],[5,3]]
+# xg = defaultdict(list)
+# yg = defaultdict(list)
+
+# for b in buildings:
+# 	x, y = b
+# 	xg[x].append(b)
+# 	yg[y].append(b)
+
+# for x in xg:
+# 	xg[x].sort(key = lambda x: x[0])
+
+# for y in yg:
+# 	yg[y].sort(key = lambda x: x[1])
+# print(xg, yg)
+
+# res = 0
+# for b in buildings:
+# 	x, y = b
+# 	if b in xg[x][1:-1] and b in yg[y][1:-1]:
+# 		res += 1
+
+# left, right = 1, len(nums)
+# while left + 1 < right:
+# 	mid = (left + right) // 2
+# 	if check(mid):
+# 		right = mid
+# 	else:
+# 		left = mid
+# 	return right
+
+# from collections import defaultdict
+
+# nums = [10000]
+# start, end = -1, -1
+# left = 0
+# hash_map = defaultdict(int)
+# for right, num in enumerate(nums):
+# 	hash_map[num] += 1
+# 	while len(hash_map) != right - left + 1:
+# 		hash_map[nums[left]] -= 1
+# 		if hash_map[nums[left]] == 0:
+# 			del hash_map[nums[left]]
+# 		left += 1
+# 	if end - start < right - left:
+# 		start, end = left, right
+# print(start, end)
